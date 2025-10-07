3e722f5746c6fb42a2a15b3d92aceec62a5f0933
James Mathitha <160629066+JamesMathitha@users.noreply.github.com>
2025-10-07 12:09:00 +0200

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Royal-Luxe/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
